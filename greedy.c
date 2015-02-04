00d94b3edfcc8340e41e57fcdead83fea4ed08c9
Scott Linne <scottlinne@gmail.com>
2015-02-03 21:49:11 -0600

    added greedy

---
+++ greedy.c
@@ -0,0 +1,24 @@
+/***********
+Scott Linne
+CS50 2015
+Greedy - Program to receive user cash input, and return number
+of coins as change
+
+
+************/
+
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+
+{
+SSSSSSSS
+}
+
+
+
+
+
+
